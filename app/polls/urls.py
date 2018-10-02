5863301ba65eb90d6337b24c5cc8f7f3ea6c9c29
mongkyo <dreamong91@gmail.com>
2018-10-02 20:51:01 +0900

    polls/urls.py
    config/urls에 include를 통해 로컬주소 이후에 /polls/라는 문자가 오면
    polls/urls에서 처리하도록 함.

---
+++ app/polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    # config.urls 에서 include로 /polls/라고 왔을 때 이 polls/urls를 참조하게 만들었으므로
+    # 아래의 식이 실행되는 경우는 http://127.0.0.1:8000/pol ls/ 이다.
+    path('', views.index, name='index'),
+]
