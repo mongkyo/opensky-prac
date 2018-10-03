e579c9746aad15e038e2790f6973b067950a6d81
mongkyo <dreamong91@gmail.com>
2018-10-03 18:24:44 +0900

    index, detail, result, vote view를 url과 연결, index뷰는 가장 최근에 발행된 Question을 5개까지 보여줌

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # config.urls 에서 include로 /polls/라고 왔을 때 이 polls/urls를 참조하게 만들었으므로
     # 아래의 식이 실행되는 경우는 http://127.0.0.1:8000/pol ls/ 이다.
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
